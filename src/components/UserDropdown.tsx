d601fa5587383062131e3d543f55fe8236841ab6
khil94 <zidru94@gmail.com>
2024-08-05 00:59:54 +0900

    [feat] : 사용자 이름을 보여주는 dropdown

---
+++ src/components/UserDropdown.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { getUserData } from "@/api/firebase";
+import { IUser } from "@/types/dataTypes";
+import { useEffect, useState } from "react";
+
+interface IProp extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  onChange: (v: any) => void;
+  placeholder?: string;
+}
+
+export default function UserDropdown({
+  onChange,
+  placeholder,
+  ...rest
+}: IProp) {
+  const [userData, setUserData] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const test = await getUserData();
+      const temp: IUser[] = [];
+      test.forEach((v) => {
+        temp.push(v.data() as IUser);
+      });
+      setUserData(temp.sort((a, b) => a.name.localeCompare(b.name)));
+    };
+    getUsers();
+  }, []);
+
+  return (
+    <select
+      {...rest}
+      className=" bg-bgSecondary p-2 pr-6 rounded-md invalid:text-gray-300"
+      onChange={(e) => {
+        onChange(e.target.value);
+      }}
+      defaultValue={""}
+    >
+      <option value="" disabled hidden>
+        {placeholder}
+      </option>
+      {userData.map((v, i) => (
+        <option key={v.id} value={v.name}>
+          {v.name}
+        </option>
+      ))}
+    </select>
+  );
+}
