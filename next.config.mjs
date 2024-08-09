ffe7bde796795e8e302c6fbfeaa72cbd7d206f0e
khil94 <zidru94@gmail.com>
2024-08-09 23:42:35 +0900

    feat : svgr 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.(js|ts)x?$/,
+
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  compiler: {
+    styledComponents: true,
+  },
+};
 
 export default nextConfig;
