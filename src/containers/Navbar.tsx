c873cd037cbab9b3a9e41b3068c025e4173cac23
khil94 <zidru94@gmail.com>
2024-08-28 01:31:40 +0900

    feat : Navbar 초안(애니메이션 등 미구현)

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -1,7 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import BurgerBar from "../../public/burger-bar.svg";
+import Home from "../../public/home.svg";
+import Record from "../../public/record.svg";
+import Records from "../../public/records.svg";
+
+import { useState } from "react";
+
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className=" w-full flex justify-center items-center bg-transparent sticky top-0 left-0 backdrop-blur-lg">
-      test
+    <div
+      className="hidden sm:w-full sm:flex justify-end 
+    items-center bg-transparent
+    sticky top-0 left-0 backdrop-blur-lg
+    border-solid border-b border-border
+    p-4"
+    >
+      <div onClick={() => setIsOpen(!isOpen)}>
+        <BurgerBar width={40} height={40} />
+      </div>
+      <div
+        onClick={() => setIsOpen(false)}
+        className={`${
+          isOpen ? `flex` : "hidden"
+        } absolute flex-col gap-8  w-full top-0 right-0 p-4 items-end bg-bgPrimary border-solid border-b border-border`}
+      >
+        <div className="" onClick={() => setIsOpen(!isOpen)}>
+          <BurgerBar width={40} height={40} />
+        </div>
+        <Link href={"/"}>
+          <Home width={50} height={50} />
+        </Link>
+        <Link href={"/record"}>
+          <Record width={50} height={50} />
+        </Link>
+        <Link href={"/records"}>
+          <Records width={50} height={50} />
+        </Link>
+      </div>
     </div>
   );
 }
