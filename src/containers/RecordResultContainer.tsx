051a2d12dba6dedb49b7a32bda41ea2be7b1b72d
khil94 <zidru94@gmail.com>
2024-08-28 22:44:43 +0900

    feat : Record 결과 화면 버튼 추가 및 스타일링
    결과화면 disabled를 위한 중복확인 함수도 추가. 우마가 0인지도 확인

--- src/containers/RecordResultContainer.tsx
+++ src/containers/RecordResultContainer.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/Button";
 import { IGameDetail, IPostGameData } from "@/types/dataTypes";
-
+import { useMemo } from "react";
 interface IProp {
   onCancel: () => void;
   umaSum: number;
@@ -18,9 +18,21 @@ export default function RecordResultContainer({
   data,
   visible,
 }: IProp) {
+  function checkDup() {
+    if (data?.detail) {
+      const temp = new Set(data.detail.map((v) => v.userName));
+      return temp.size !== 4;
+    }
+    return false;
+  }
+
+  const userDup = useMemo(() => {
+    return checkDup();
+  }, [data]);
+
   function UserDataComp({ userData }: { userData: IGameDetail }) {
     return (
-      <tr className="w-full bg-red-300 ">
+      <tr className="w-full bg-border">
         <td>{userData.rank}</td>
         <td>{userData.userName}</td>
         <td>{userData.score}</td>
@@ -31,29 +43,52 @@ export default function RecordResultContainer({
 
   return (
     <div
-      className={`${visibleProp[visible]} flex justify-center items-center flex-col w-full`}
+      className={`${visibleProp[visible]} w-full flex justify-center items-center flex-col gap-8 `}
     >
-      <table className=" text-center">
-        <thead>
-          <tr className="[&_th]:p-4">
+      <h1>입력 결과</h1>
+      <table className="overflow-x-scroll text-center min-w-[40%] ">
+        <colgroup>
+          <col width={"20%"} />
+          <col width={"30%"} />
+          <col width={"30%"} />
+          <col width={"20%"} />
+        </colgroup>
+
+        <thead className="bg-bgSecondary">
+          <tr className="[&_th]:p-2 ">
             <th>순위</th>
             <th>이름</th>
             <th>점수</th>
             <th>우마</th>
           </tr>
         </thead>
-        <tbody className="[&_td]:p-4 [&_td]:px-8">
+        <tbody className="[&_td]:p-4">
           {data !== undefined &&
             data.detail.map((v) => {
               return <UserDataComp userData={v} key={v.userName + v.uma} />;
             })}
         </tbody>
       </table>
-      <Button
-        colorType={"border"}
-        onClick={onCancel}
-        text={`우마는 ${umaSum}`}
-      />
+      <div className="flex flex-col justify-center items-center gap-8">
+        <span>{`우마는 ${umaSum}`}</span>
+        {umaSum !== 0 && <span>우마를 확인해주세요.</span>}
+        {userDup && <span>사용자를 확인해주세요.</span>}
+        <div className="flex flex-row gap-8">
+          <Button
+            sizeType="sm"
+            colorType={"border"}
+            onClick={onCancel}
+            text={`뒤로가기`}
+          />
+          <Button
+            sizeType="sm"
+            colorType={"main"}
+            disabled={umaSum !== 0 || userDup}
+            onClick={onCancel}
+            text={"제출"}
+          />
+        </div>
+      </div>
     </div>
   );
 }
