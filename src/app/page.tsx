d55efa921f8b60d33f7432aa8c5ec8cacbb37b58
khil94 <zidru94@gmail.com>
2024-08-30 03:39:11 +0900

    feat : 메인 페이지 추가
    메인 페이지에는 유저 검색창을 삽입.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+import Button from "@/components/Button";
+import UserDropdown from "@/components/UserDropdown";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Home() {
+  const [searchVal, setSearchVal] = useState("");
+
   return (
-    <main className="flex flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center justify-between w-full p-8">
+      <div className="flex flex-row w-1/2 md:w-full gap-4">
+        <UserDropdown
+          customClass=" w-full "
+          onChange={(val) => {
+            setSearchVal(val);
+          }}
+          placeholder="사용자 선택"
+        />
+        <Link href={`/user/${searchVal}`}>
+          <Button sizeType="sm" text="검색" />
+        </Link>
+      </div>
+    </main>
   );
 }
