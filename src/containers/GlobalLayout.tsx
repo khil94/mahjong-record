fc52f06667d23585f8a360b42d9085814ef2330b
khil94 <zidru94@gmail.com>
2024-08-30 05:36:51 +0900

    fix : height 100% 관련 문제 해결
    html body에 height 100%가 누락되어 있을 뿐 아니라 GLayout에도 누락되어 있어 수정

--- src/containers/GlobalLayout.tsx
+++ src/containers/GlobalLayout.tsx
@@ -7,10 +7,10 @@ export default function GlobalLayout({
   children?: React.ReactNode;
 }) {
   return (
-    <div className={` bg-bgPrimary text-text`}>
+    <div className={`h-full bg-bgPrimary text-text`}>
       <Navbar />
       <Sidebar />
-      <div className="min-h-dvh h-full flex justify-center items-center pl-24 sm:pl-0 sm:pt-24 ">
+      <div className="h-full flex justify-center items-center pl-24 sm:pl-0 sm:pt-24 ">
         {children}
       </div>
     </div>
