9767ec298c76dca12754473ea76100aaf78d2715
khil94 <zidru94@gmail.com>
2024-08-30 09:26:33 +0900

    feat : ChartWrapper 제작
    MultiTabLayout 을 통해 렌더링할 client component

---
+++ src/app/records/components/ChartWrapper.tsx
@@ -0,0 +1,24 @@
+"use client";
+import MultitabLayout from "@/containers/MultitabLayout";
+import { IGameData, IUser } from "@/types/dataTypes";
+import GameHistory from "./GameHistory";
+import RankDataChart from "./RankDataChart";
+import UserUmaChart from "./UserUmaChart";
+
+interface IProp {
+  gameData: IGameData[];
+  userData: IUser[];
+}
+
+export default function ChartWrapper({ gameData, userData }: IProp) {
+  return (
+    <MultitabLayout
+      tabList={["리더보드", "우마 그래프", "게임 기록"]}
+      tabCompList={[
+        <RankDataChart userData={userData} />,
+        <UserUmaChart userData={userData} />,
+        <GameHistory gameData={gameData} />,
+      ]}
+    />
+  );
+}
