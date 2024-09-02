4a7c2fb0d931f65ed79e482860369603ced6c000
khil94 <zidru94@gmail.com>
2024-09-03 07:09:35 +0900

    feat : Records Page에 onSnapshot 추가
    우선 시범적으로Records 페이지에 onSnapshot을 이용한 실시간 데이터 구독 시스템 추가

--- src/app/records/components/ChartWrapper.tsx
+++ src/app/records/components/ChartWrapper.tsx
@@ -1,6 +1,10 @@
 "use client";
+import { db } from "@/api/firebase";
+import Loading from "@/components/Loading";
 import MultitabLayout from "@/containers/MultitabLayout";
 import { IGameData, IUser } from "@/types/dataTypes";
+import { collection, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import GameHistory from "../../../components/GameHistory";
 import RankDataChart from "./RankDataChart";
 import UserUmaChart from "./UserUmaChart";
@@ -11,13 +15,52 @@ interface IProp {
 }
 
 export default function ChartWrapper({ gameData, userData }: IProp) {
-  return (
+  const [liveGameData, setLiveGameData] = useState<IGameData[]>(gameData);
+  const [liveUserData, setLiveUserData] = useState<IUser[]>(userData);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // 게임 데이터 실시간 구독
+    const unsubscribeGameData = onSnapshot(
+      collection(db, "games"),
+      (snapshot) => {
+        const updatedGameData = snapshot.docs.map(
+          (doc) => doc.data() as IGameData
+        );
+        setLiveGameData(updatedGameData);
+        setLoading(false);
+      }
+    );
+
+    // 유저 데이터 실시간 구독
+    const unsubscribeUserData = onSnapshot(
+      collection(db, "users"),
+      (snapshot) => {
+        const updatedUserData = snapshot.docs.map((doc) => doc.data() as IUser);
+        setLiveUserData(updatedUserData);
+        setLoading(false);
+      }
+    );
+
+    // 컴포넌트 언마운트 시 구독 해제
+    return () => {
+      unsubscribeGameData();
+      unsubscribeUserData();
+    };
+  }, []);
+
+  return loading ? (
+    <Loading size={50} />
+  ) : (
     <MultitabLayout
       tabList={["리더보드", "우마 그래프", "게임 기록"]}
       tabCompList={[
-        <RankDataChart key={`multitab-rank-data`} userData={userData} />,
-        <UserUmaChart key={`multitab-user-leaderboard`} userData={userData} />,
-        <GameHistory key={`multitab-game-history`} gameData={gameData} />,
+        <RankDataChart key={`multitab-rank-data`} userData={liveUserData} />,
+        <UserUmaChart
+          key={`multitab-user-leaderboard`}
+          userData={liveUserData}
+        />,
+        <GameHistory key={`multitab-game-history`} gameData={liveGameData} />,
       ]}
     />
   );
