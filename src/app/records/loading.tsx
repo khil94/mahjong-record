ab86a8619b625dd4c4d5db2e6b6c0eaaf4ef9ca1
khil94 <zidru94@gmail.com>
2024-09-03 17:57:10 +0900

    feat : 각 페이지 진입시 로딩될 때 까지 보여줄 로딩 comp

---
+++ src/app/records/loading.tsx
@@ -0,0 +1,9 @@
+import Loading from "@/components/Loading";
+
+export default function RecordsLoading() {
+  return (
+    <div>
+      <Loading size={50} />
+    </div>
+  );
+}
---
+++ src/app/score-table/loading.tsx
@@ -0,0 +1,9 @@
+import Loading from "@/components/Loading";
+
+export default function ScoreTableLoading() {
+  return (
+    <div>
+      <Loading size={50} />
+    </div>
+  );
+}
