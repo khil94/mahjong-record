01d5446d33ece9f8d57b7c90567396a8b702c5d6
khil94 <zidru94@gmail.com>
2024-09-05 20:28:47 +0900

    feat : useTheme cookie를 이용하지 않게 수정
    cookie를 이용하면 상태를 저장할 수있으나, 기기의 테마와 다르면 깜빡거리는 현상이 있어 삭제

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -2,7 +2,6 @@
 import { changeTheme } from "@/lib/features/theme/themeSlice";
 import { RootState } from "@/lib/store";
 import { ITheme } from "@/types/dataTypes";
-import { getCookie, setCookie } from "cookies-next";
 import { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -13,23 +12,16 @@ const useTheme = () => {
     (val: ITheme) => {
       document.documentElement.setAttribute("data-theme", val);
       dispatch(changeTheme(val));
-      setCookie("theme", val);
     },
     [dispatch]
   );
 
   const initiateState = useCallback(() => {
-    const cookieTheme = getCookie("theme");
-
-    if (cookieTheme) {
-      setTheme(cookieTheme as ITheme);
-    } else {
-      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-        .matches
-        ? "dark"
-        : ("light" as ITheme);
-      setTheme(systemTheme);
-    }
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+      .matches
+      ? "dark"
+      : ("light" as ITheme);
+    setTheme(systemTheme);
   }, [setTheme]);
 
   const toggleTheme = useCallback(() => {
