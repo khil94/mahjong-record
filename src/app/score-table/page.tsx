ac5c2b226334aa729a5bb2b28300dabf8ca406d6
khil94 <zidru94@gmail.com>
2024-09-05 18:57:19 +0900

    refact : img 태그를 next의 Image태그로 대체

--- src/app/score-table/page.tsx
+++ src/app/score-table/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function ScoreTablePage() {
   return (
-    <div className="w-full h-full gap-4 p-4 flex flex-col justify-start items-center">
+    <div className="w-full h-full gap-4 p-4 flex flex-col justify-start items-center md:pt-24">
       <h1 className="text-3xl font-bold">점수표</h1>
-      <img src="/score-table.png" />
+      <Image
+        width={600}
+        height={0}
+        style={{ height: "auto" }}
+        alt="score-table"
+        src="/score-table.png"
+        priority
+      />
     </div>
   );
 }
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 interface IProp {
   size: number;
 }
 
 export default function Loading({ size }: IProp) {
-  return <img src="/spinner.svg" width={size} />;
+  return (
+    <Image
+      src="/spinner.svg"
+      width={size}
+      height={0}
+      style={{ height: "auto" }}
+      alt="loading"
+    />
+  );
 }
